fda7bdb91af03d681e26a82786ba85ecfeb1c936
Alan Yeo <me@alanyjw.com>
2016-08-23 16:37:28 +0800

    Further refactor for set logic and format code

--- pangram/Pangram.elm
+++ pangram/Pangram.elm
@@ -5,20 +5,30 @@ import List exposing (map)
 import Set exposing (..)
 import String exposing (..)
 
-lowerAlphaCodeSet = Set.fromList [97..122]
+
+lowerAlphaCodeSet =
+    Set.fromList [97..122]
+
 
 isPangram : String -> Bool
 isPangram sentence =
-  let
-      toLowerCodeSet = (
-        \s -> s
-          |> String.toLower
-          |> String.toList
-          |> List.map Char.toCode
-          |> Set.fromList
-      )
-  in
-      sentence
-        |> toLowerCodeSet
-        |> Set.diff lowerAlphaCodeSet
-        |> Set.isEmpty
+    let
+        toLowerCodeSet =
+            (\s ->
+                s
+                    |> String.toLower
+                    |> String.toList
+                    |> List.map Char.toCode
+                    |> Set.fromList
+            )
+
+        hasAtLeastOneAlphaCode =
+            (\set ->
+                set
+                    |> Set.diff lowerAlphaCodeSet
+                    |> Set.isEmpty
+            )
+    in
+        sentence
+            |> toLowerCodeSet
+            |> hasAtLeastOneAlphaCode
