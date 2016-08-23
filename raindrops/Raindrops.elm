0d31e8102b8a870300ac534b07eb6749b93de47d
Alan Yeo <me@alanyjw.com>
2016-08-23 13:59:43 +0800

    Complete Raindrops

--- raindrops/Raindrops.elm
+++ raindrops/Raindrops.elm
@@ -1,1 +1,31 @@
 module Raindrops exposing (..)
+
+import List exposing (..)
+import String exposing (..)
+
+divides : Int -> Int -> Bool
+divides dividend divisor =
+  dividend % divisor == 0
+
+pling : Int -> String
+pling number =
+  if number `divides` 3 then "Pling" else ""
+
+plang : Int -> String
+plang number =
+  if number `divides` 5 then "Plang" else ""
+
+plong : Int -> String
+plong number =
+  if number `divides` 7 then "Plong" else ""
+
+default : Int -> String
+default number =
+  if not (number `divides` 3) && not (number `divides` 5) && not (number `divides` 7) then
+    toString number
+  else
+    ""
+
+raindrops : Int -> String
+raindrops number =
+  pling number ++ plang number ++ plong number ++ default number
