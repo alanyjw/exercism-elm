e88afa2a391b2288b85b0d1b77c476c487858e82
Alan Yeo <me@alanyjw.com>
2016-08-23 16:05:34 +0800

    Complete Pangram

--- pangram/Pangram.elm
+++ pangram/Pangram.elm
@@ -1,1 +1,16 @@
 module Pangram exposing (..)
+
+import Char exposing (toCode)
+import List exposing (map)
+import Set exposing (..)
+import String exposing (..)
+
+isPangram : String -> Bool
+isPangram sentence =
+  sentence
+    |> String.toLower
+    |> String.toList
+    |> List.map Char.toCode
+    |> Set.fromList
+    |> Set.diff (Set.fromList [97..122])
+    |> Set.isEmpty
