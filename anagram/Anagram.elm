52126c59941ed1e95d841a32ce900c044b7f73c4
Alan Yeo <me@alanyjw.com>
2016-09-13 23:52:18 +0800

    Complete Anagram

--- anagram/Anagram.elm
+++ anagram/Anagram.elm
@@ -1,1 +1,24 @@
 module Anagram exposing (..)
+
+import List exposing (sort)
+import String exposing (split,toLower)
+
+isAnagram : String -> String -> Bool
+isAnagram first second =
+  let
+      lowercasedFirst = toLower first
+      lowercasedSecond = toLower second
+      sortChars = \s -> sort (split "" s)
+  in
+      lowercasedFirst /= lowercasedSecond &&
+        (sortChars lowercasedFirst) == (sortChars lowercasedSecond)
+
+detect : String -> List String -> List String
+detect word listOfWords =
+  case listOfWords of
+    [] -> []
+    head::tail ->
+      if isAnagram word head then
+        head :: detect word tail
+      else
+        detect word tail
