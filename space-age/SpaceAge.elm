5f7fe4651d949cfeb63f7c7fc7df3ed7e4db8b42
Alan Yeo <me@alanyjw.com>
2016-09-15 16:51:09 +0800

    Complete Space Age

--- space-age/SpaceAge.elm
+++ space-age/SpaceAge.elm
@@ -9,3 +9,19 @@ type Planet
   | Saturn
   | Uranus
   | Neptune
+
+earthYearsOn : Planet -> Float
+earthYearsOn planet =
+  case planet of
+    Mercury -> 0.2408467
+    Venus -> 0.61519726
+    Earth -> 1
+    Mars -> 1.8808158
+    Jupiter -> 11.862615
+    Saturn -> 29.447498
+    Uranus -> 84.016846
+    Neptune -> 164.79132
+
+ageOn : Planet -> Float -> Float
+ageOn planet age =
+  age / 31557600 / (earthYearsOn planet)
